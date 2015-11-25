210e6ee7e089f744df9d956ad3e358ce19255be4
Donne Martin <donne.martin@gmail.com>
2015-11-24 21:35:36 -0500

    Reformat onions.

--- hncli/onions.py
+++ hncli/onions.py
@@ -13,73 +13,49 @@
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
 
-lulz = [
+onions = [
     u'Developer Accused Of Unreadable Code Refuses To Comment.',
     u'Top 9 Reasons Arrays Are Confusing, You Won’t Believe #0.',
-    u'26 Variable Names For Busy Developers:              \n'
-     'a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z.',
-    u'Datacenter 1 Uptime: 94.6%, Datacenter 2 Uptime: 92.0%,\n'
-     'Cloud Sales Rep: "We have 186.6% Uptime."              ',
-    u'Hackathon Developer Gets Development Environment Working \n'
-     'With Two Whole Hours To Go.                              ',
+    u'26 Variable Names For Busy Developers: a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z.',
+    u'Datacenter 1 Uptime: 94.6%, Datacenter 2 Uptime: 92.0%, Cloud Sales Rep: "We have 186.6% Uptime."',
+    u'Hackathon Developer Gets Development Environment Working With Two Whole Hours To Go.',
     u'Recruiter Confident Java Developer Can Learn The Script Part.',
     u"Ops Guy Doesn't Seem To Care That Code Worked Locally.",
-    u'Google announces 93% of users are now on one of the last\n'
-     '16 versions of Android #io14 #GoogleIO2014 #AndroidOne. ',
-    u'Developer who inherited 5-year-old Rails codebase\n'
-     'secretly hoping for company collapse.            ',
-    u'More realistic Java Garbage Collector runs only on   \n'
-     'Tuesdays at 4am and makes an obscene amount of noise.',
-    u'Daft Punk admits #1 hit song "Up All Night To Get Lucky"\n'
-     'is actually about programming.                          ',
+    u'Google announces 93% of users are now on one of the last 16 versions of Android #io14 #GoogleIO2014 #AndroidOne.',
+    u'Developer who inherited 5-year-old Rails codebase secretly hoping for company collapse.',
+    u'More realistic Java Garbage Collector runs only on Tuesdays at 4am and makes an obscene amount of noise.',
+    u'Daft Punk admits #1 hit song "Up All Night To Get Lucky" is actually about programming.',
     u'Area developer breaks own leg to get out of daily standup.',
     u'New node.js co-working space has 1 table and everyone takes turns.',
     u'New Github IDE can tab-complete an entire Rails app.',
     u'Predictive analytics startup uses own software to predict lack of adoption.',
-    u'Top 5 World Languages:                                   \n'
-     '1: Mandarin 2: English 3: Spanish 4: Hindi 5: JavaScript.',
-    u'A gorgeous, opinionated JavaScript microframework \n'
-     'hand-delivered to your door every month.          ',
+    u'Top 5 World Languages: 1. Mandarin 2. English 3. Spanish 4. Hindi 5. JavaScript.',
+    u'A gorgeous, opinionated JavaScript microframework hand-delivered to your door every month.',
     u'VPNs are pieces of SSH IT.',
-    u'Q: What do you call a VC who writes code? \n'
-     'A: A Rich Text Editor.                    ',
-    u'My boss loved The Mythical Man Month.                              \n'
-     "He gave me 2 copies and said: 'here, it'll take you half the time.'",
-    u'Hacker News is DOWN, but your chances of getting into YC\n'
-     'if you know how to scale a plain text website are UP.   ',
+    u'Q: What do you call a VC who writes code? A: A Rich Text Editor.',
+    u"My boss loved The Mythical Man Month. He gave me 2 copies and said 'here, it'll take you half the time.'",
+    u'Hacker News is DOWN, but your chances of getting into YC if you know how to scale a plain text website are UP.',
     u'TIL the movie Inception was based on node.js callbacks.',
-    u'Getting the NSA a present this year?         \n'
-     "Don't spoil the surprise by buying it online.",
+    u"Getting the NSA a present this year? Don't spoil the surprise by buying it online.",
     u'"Losing data is a feature." – Why Snapchat loves MongoDB.',
     u'Leaked NSA source code contains no private methods.',
     u'Startup naming trends: -ster 1999, -ify 2006, -ly 2009, .io 2013.',
     u"BREAKING: Rails 5 will run each HTTP request in it's own Docker container.",
     u'Programming is 1% inspiration, 99% trying to get your environment working.',
-    u'Microsoft board members vote 4-3 to temporarily raise the\n'
-     'technical debt ceiling so work on Windows 8 can continue.',
+    u'Microsoft board members vote 4-3 to temporarily raise the technical debt ceiling so work on Windows 8 can continue.',
     u'Our retention strategy is Stockholm Syndrome.',
-    u'OkCupid has suspended Martin Odersky. The creator of Scala uploaded a\n'
-     "37,000 word answer to profile question 'What is your type?'          ",
-    u'MongoDB adds a "PLEASE" keyword for inserts,     \n'
-     'boosting chance that data is stored to above 75%.',
+    u"OkCupid has suspended Martin Odersky. The creator of Scala uploaded a 37,000 word answer to profile question 'What is your type?'",
+    u'MongoDB adds a "PLEASE" keyword for inserts, boosting chance that data is stored to above 75%.',
     u'Self-proclaimed "visionary" seeks technical cofounder.',
-    u'Obama – "The national technical debt is increasing at an alarming rate.\n'
-     'We have to stop writing PHP."                                          ',
-    u'99% of programming is choosing a templating language and\n'
-     'the rest is explaining your decision in a blog post.    ',
-    u'$ git diff: + <meta content="width=device-width; initial-scale=1.0" />\n'
-     '$ git commit -am "Mobile ready".                                      ',
+    u'Obama – "The national technical debt is increasing at an alarming rate. We have to stop writing PHP."',
+    u'99% of programming is choosing a templating language and the rest is explaining your decision in a blog post.',
+    u'$ git diff + <meta content="width=device-width; initial-scale=1.0" /> $ git commit -am "Mobile ready".',
     u"Hotshot systems architect seeks maintenance guy to take over last year's project.",
     u'JavaScript library with no code but cool logo hits 5,000 stars.',
     u'Have to write SQL? SELECT * FROM STACK OVERFLOW.',
-    u'Novice programmers swear when they get stuck. \n'
-     'Expert programmers swear every fucking second.',
-    u'Enterprise CTO not impressed by pair programming.                \n'
-     '"We often have hundreds of developers working on the same thing."',
-    u'Q: Why is Java slow?      \n'
-     'A: Because Time is a Long.',
-    u'$ git push --force                               \n'
-     "I got 99 problems but a merge conflict ain't one.",
-    u'Recruiter mistakenly hires real ninja.                                                   \n'
-     'Team finds this totally sweet until ninja commits seppuku when told to use graphical IDE.',
+    u'Novice programmers swear when they get stuck. Expert programmers swear every fucking second.',
+    u'Enterprise CTO not impressed by pair programming. "We often have hundreds of developers working on the same thing."',
+    u'Q: Why is Java slow? A: Because Time is a Long.',
+    u"`git push --force` I got 99 problems but a merge conflict ain't one.",
+    u'Recruiter mistakenly hires real ninja. Team finds this totally sweet until ninja commits seppuku when told to use graphical IDE.',
 ]
