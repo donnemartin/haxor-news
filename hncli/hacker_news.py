3c521ba3053a4cec92f078b9517db240969deba0
Donne Martin <donne.martin@gmail.com>
2015-11-27 06:10:34 -0500

    Add pretty_date_time method.

--- hncli/hacker_news.py
+++ hncli/hacker_news.py
@@ -83,6 +83,56 @@ class HackerNews(object):
         config_file_path = os.path.join(home, config_file_name)
         return config_file_path
 
+    def pretty_date_time(self, date_time):
+        """Prints a pretty datetime similar to what's seen on Hacker News.
+
+        Gets a datetime object or a int() Epoch timestamp and return a
+        pretty string like 'an hour ago', 'Yesterday', '3 months ago',
+        'just now', etc.
+
+        Adapted from: http://stackoverflow.com/questions/1551382/user-friendly-time-format-in-python
+
+        Args:
+            * date_time: An instance of datetime.
+
+        Returns:
+            A string that represents the pretty datetime.
+        """
+        from datetime import datetime
+        now = datetime.now()
+        if type(date_time) is int:
+            diff = now - datetime.fromtimestamp(date_time)
+        elif isinstance(date_time,datetime):
+            diff = now - date_time
+        elif not date_time:
+            diff = now - now
+        second_diff = diff.seconds
+        day_diff = diff.days
+        if day_diff < 0:
+            return ''
+        if day_diff == 0:
+            if second_diff < 10:
+                return "just now"
+            if second_diff < 60:
+                return str(second_diff) + " seconds ago"
+            if second_diff < 120:
+                return "1 minute ago"
+            if second_diff < 3600:
+                return str(second_diff // 60) + " minutes ago"
+            if second_diff < 7200:
+                return "1 hour ago"
+            if second_diff < 86400:
+                return str(second_diff // 3600) + " hours ago"
+        if day_diff == 1:
+            return "Yesterday"
+        if day_diff < 7:
+            return str(day_diff) + " days ago"
+        if day_diff < 31:
+            return str(day_diff // 7) + " weeks ago"
+        if day_diff < 365:
+            return str(day_diff // 30) + " months ago"
+        return str(day_diff // 365) + " years ago"
+
     def print_comments(self, item, regex_query='', depth=0):
         comment_ids = item.kids
         if item.text is not None:
