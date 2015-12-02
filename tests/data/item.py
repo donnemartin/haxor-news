c1183d79a8d8676d9ce6ea32a7053a4fd08cd553
Donne Martin <donne.martin@gmail.com>
2015-12-02 05:18:32 -0500

    Add item test data.

---
+++ tests/data/item.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2015 Donne Martin. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+
+formatted_items = [
+    '\x1b[35m  1.  \x1b[0m\x1b[34mtitle foo \x1b[0m\x1b[35m()\x1b[0m\n\x1b[32m      10 points \x1b[0m\x1b[33mby foo \x1b[0m\x1b[36mjust now \x1b[0m\x1b[32m| 2 comments\n\x1b[0m',
+    '\x1b[35m  2.  \x1b[0m\x1b[34mtitle bar \x1b[0m\x1b[35m()\x1b[0m\n\x1b[32m      20 points \x1b[0m\x1b[33mby bar \x1b[0m\x1b[36mjust now \x1b[0m\x1b[32m| 1 comments\n\x1b[0m',
+    '\x1b[35m  3.  \x1b[0m\x1b[34mtitle baz \x1b[0m\x1b[35m()\x1b[0m\n\x1b[32m      30 points \x1b[0m\x1b[33mby baz \x1b[0m\x1b[36mjust now \x1b[0m\x1b[32m| 0 comments\n\x1b[0m',
+]
