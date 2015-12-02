5539f464f7dc0ddbfa22af4d032f917c001c3c70
Donne Martin <donne.martin@gmail.com>
2015-12-02 05:18:18 -0500

    Add data module init along with comment test data.

---
+++ tests/data/comment.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2015 Donne Martin. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+
+raw_comment = u"""I downvoted you, but I thought I&#x27;d explain why - I don&#x27;t think it&#x27;s reasonable to characterise this as childish.  Government surveillance is something which our industry is the best positioned to speak out against.  This type of thing seems to be clearly political speech, and not a prank."""
+
+formatted_heading = u'\x1b[33m\n            foo - just now\n\x1b[0m'
+
+formatted_comment = u"""            I downvoted you, but I thought I&#x27;d explain why - I don&#x27;t\n            think it&#x27;s reasonable to characterise this as childish.\n            Government surveillance is something which our industry is the\n            best positioned to speak out against.  This type of thing seems to\n            be clearly political speech, and not a prank."""
