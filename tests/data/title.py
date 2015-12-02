36410ccb915b849d2b2aa46ede1eb8531797668a
Donne Martin <donne.martin@gmail.com>
2015-12-02 05:19:14 -0500

    Add title test data.

---
+++ tests/data/title.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2015 Donne Martin. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+
+raw_title = u'The secret message hidden in every HTTP/2 connection'
+
+formatted_title = u'\x1b[35m  1.  \x1b[0m\x1b[34mThe secret message hidden in every HTTP/2 connection \x1b[0m'
