cd119537251c90d5f8659da3cae121622b289f5b
Donne Martin <donne.martin@gmail.com>
2015-12-02 05:19:01 -0500

    Add tip test data.

---
+++ tests/data/tip.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2015 Donne Martin. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+
+formatted_tip = '\x1b[34mTip: View the page or comments for \x1b[0m\x1b[35m1 through \x1b[0m\x1b[35m10\x1b[0m\x1b[34m with the following command:\n\x1b[0m\x1b[34m  hn view [#] [comment_filter] [-c] [-cr] [-b] - hn view --help\n\x1b[0m'
