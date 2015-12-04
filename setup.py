7230eaa76683df11faac8a92ef345d35c92ae44a
Donne Martin <donne.martin@gmail.com>
2015-12-04 06:42:21 -0500

    Add pygments and prompt-toolkit to packaging.

--- setup.py
+++ setup.py
@@ -17,6 +17,8 @@ setup(
         'colorama>=0.3.3',
         'html2text==2015.11.4',
         'requests>=2.4.3',
+        'pygments>=2.0.2',
+        'prompt-toolkit==0.52',
         'six>=1.9.0',
     ],
     extras_require={
