5d27e1fab74e83dabdeb00549d029974686685cf
Donne Martin <donne.martin@gmail.com>
2016-03-13 06:55:41 -0400

    Update linter excludes

--- scripts/run_code_checks.sh
+++ scripts/run_code_checks.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-flake8 --max-line-length=80 --exclude=build,scratch,docs,haxor,onions.py .
+flake8 --max-line-length=80 --exclude=build,docs,scratch,docs,haxor,html2text,onions.py,compat.py .
