ae0bd13f111e8d849930944dfdde4bfca2659f5a
Donne Martin <donne.martin@gmail.com>
2016-04-03 07:54:45 -0400

    Remove html2text pip requirement, now bundled

--- setup.py
+++ setup.py
@@ -17,7 +17,6 @@ setup(
     install_requires=[
         'click>=5.1',
         'colorama>=0.3.3',
-        'html2text==2015.11.4',
         'requests>=2.4.3',
         'pygments>=2.0.2',
         'prompt-toolkit==0.52',
