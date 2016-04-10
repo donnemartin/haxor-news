2f97900305c863bf64add3523f22eab5a21b4ccb
Donne Martin <donne.martin@gmail.com>
2016-04-10 12:36:59 -0400

    Fix #32: Add a user agent string to access sites that require it

--- haxor_news/web_viewer.py
+++ haxor_news/web_viewer.py
@@ -104,7 +104,8 @@ class WebViewer(object):
         :return: The string representation of the formatted url contents.
         """
         try:
-            raw_response = requests.get(url)
+            headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}  # NOQA
+            raw_response = requests.get(url, headers=headers)
         except (requests.exceptions.SSLError,
                 requests.exceptions.ConnectionError) as e:
             contents = 'Error: ' + str(e) + '\n'
