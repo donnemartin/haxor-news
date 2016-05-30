d186ae9c13a2269bd21d80fad8f1b1bc70cfebae
Donne Martin <donne.martin@gmail.com>
2016-05-30 05:43:10 -0400

    Update to latest prompt-toolkit v1.0.0

--- haxor_news/style.py
+++ haxor_news/style.py
@@ -16,7 +16,7 @@
 from pygments.token import Token
 from pygments.style import Style
 from pygments.util import ClassNotFound
-from prompt_toolkit.styles import default_style_extensions
+from prompt_toolkit.styles import default_style_extensions, style_from_dict
 import pygments.styles
 
 
@@ -46,31 +46,27 @@ class StyleFactory(object):
         except ClassNotFound:
             style = pygments.styles.get_style_by_name('native')
 
-        class CliStyle(Style):
-            """Provides styles for the autocomplete menu and the toolbar.
+        # Create styles dictionary.
+        styles = {}
+        styles.update(style.styles)
+        styles.update(default_style_extensions)
+        styles.update({
+            Token.Menu.Completions.Completion.Current: 'bg:#00aaaa #000000',
+            Token.Menu.Completions.Completion: 'bg:#008888 #ffffff',
+            Token.Menu.Completions.Meta.Current: 'bg:#00aaaa #000000',
+            Token.Menu.Completions.Meta: 'bg:#00aaaa #ffffff',
+            Token.Menu.Completions.ProgressButton: 'bg:#003333',
+            Token.Menu.Completions.ProgressBar: 'bg:#00aaaa',
+            Token.Scrollbar: 'bg:#00aaaa',
+            Token.Scrollbar.Button: 'bg:#003333',
+            Token.Toolbar: 'bg:#222222 #cccccc',
+            Token.Toolbar.Off: 'bg:#222222 #696969',
+            Token.Toolbar.On: 'bg:#222222 #ffffff',
+            Token.Toolbar.Search: 'noinherit bold',
+            Token.Toolbar.Search.Text: 'nobold',
+            Token.Toolbar.System: 'noinherit bold',
+            Token.Toolbar.Arg: 'noinherit bold',
+            Token.Toolbar.Arg.Text: 'nobold'
+        })
 
-            :type styles: dict
-            :param styles: Contains pygments style info.
-            """
-
-            styles = {}
-            styles.update(style.styles)
-            styles.update(default_style_extensions)
-            styles.update({
-                Token.Menu.Completions.Completion.Current: 'bg:#00aaaa #000000',
-                Token.Menu.Completions.Completion: 'bg:#008888 #ffffff',
-                Token.Menu.Completions.Meta.Current: 'bg:#00aaaa #000000',
-                Token.Menu.Completions.Meta: 'bg:#00aaaa #ffffff',
-                Token.Menu.Completions.ProgressButton: 'bg:#003333',
-                Token.Menu.Completions.ProgressBar: 'bg:#00aaaa',
-                Token.Toolbar: 'bg:#222222 #cccccc',
-                Token.Toolbar.Off: 'bg:#222222 #696969',
-                Token.Toolbar.On: 'bg:#222222 #ffffff',
-                Token.Toolbar.Search: 'noinherit bold',
-                Token.Toolbar.Search.Text: 'nobold',
-                Token.Toolbar.System: 'noinherit bold',
-                Token.Toolbar.Arg: 'noinherit bold',
-                Token.Toolbar.Arg.Text: 'nobold'
-            })
-
-        return CliStyle
+        return style_from_dict(styles)
