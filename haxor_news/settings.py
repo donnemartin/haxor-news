c94ae2aef114e424c65eacf404a77d8803309345
Donne Martin <donne.martin@gmail.com>
2016-11-02 03:39:29 -0400

    Fix #78: Update latest monthly hiring post ids (#79)

--- haxor_news/settings.py
+++ haxor_news/settings.py
@@ -14,5 +14,5 @@
 # language governing permissions and limitations under the License.
 
 # Updated monthly, see HackerNewsCli.hiring docstring.
-who_is_hiring_post_id = 12627852
-freelancer_post_id = 12627853
+who_is_hiring_post_id = 12846216
+freelancer_post_id = 12846145
