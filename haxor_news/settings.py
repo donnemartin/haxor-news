3ff5550f077d7dca48ab7b748b13a30862c050c2
thoo <theinhtay@gmail.com>
2018-11-01 12:00:45 -0400

    November update

--- haxor_news/settings.py
+++ haxor_news/settings.py
@@ -14,5 +14,5 @@
 # language governing permissions and limitations under the License.
 
 # Updated monthly, see HackerNewsCli.hiring docstring.
-who_is_hiring_post_id = 18113144
-freelancer_post_id = 18113145
+who_is_hiring_post_id = 18354503
+freelancer_post_id = 18354504
