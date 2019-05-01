a5ccefe78470c1c09f2024a1de08e45412545225
Kalle Laine <pahakalle@protonmail.ch>
2019-05-02 00:50:24 +0300

    Update latest monthly hiring post ids

--- haxor_news/settings.py
+++ haxor_news/settings.py
@@ -14,5 +14,5 @@
 # language governing permissions and limitations under the License.
 
 # Updated monthly, see HackerNewsCli.hiring docstring.
-who_is_hiring_post_id = 19543940
-freelancer_post_id = 19543939
+who_is_hiring_post_id = 19797594
+freelancer_post_id = 19797593
